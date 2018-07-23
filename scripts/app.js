8bea5d1b003f69694092a1c932b25d444864510b
OwenAndronicusMartin <owen@enkaizen.com>
2018-07-23 11:39:25 +0600

    app.js modified ...

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+var budgetController = (function () {
+    
+})();
+
+// User Interface control
+var UIController = (function () {
+    
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+    
+})(budgetController, UIController);
