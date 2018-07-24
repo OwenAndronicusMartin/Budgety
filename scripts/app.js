5ebb4eb96f6c976ca56f820601bc28d7365960e2
Owen Andronicus Martin <owen@enkaizen.com>
2018-07-24 17:23:30 +0600

    loot of changes ...

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,86 @@
-var budgetController = (function () {
+// BUDGET CONTROLLER
+var budgetController = (function() {
+    
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
     
 })();
 
-// User Interface control
-var UIController = (function () {
+var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+};
     
+
+// UI CONTROLLER
+var UIController = (function() {
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    }
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value,    
+            };
+        },
+        getDOMStrings: function() {
+            return DOMStrings;
+        },
+    };
 })();
 
-var controller = (function (budgetCtrl, UICtrl) {
+// GLOBAL APP CONTROLLER 
+var controller = (function(budgetCtrl, UICtrl) {
+    // Initialize event listeners
+    var setupEventListeners = function() {
+        // Get DOM elements
+        var DOM = UICtrl.getDOMStrings();
+        
+        // Button click event listeners
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        
+        // Keypress, keyup, keydown event listeners
+        document.addEventListener('keypress', function(event) {
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
     
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        
+        // 2. Add the item to the budget controller
+        
+        // 3. Add the item to the UI
+        
+        // 4. Calculate the budget
+        
+        // 5. Display the budget on the UI
+    };
+    
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    }
 })(budgetController, UIController);
+
+controller.init();
