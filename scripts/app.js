2309d4e878114fc16cc319baaf742cb8d530f124
Owen Andronicus Martin <owen@enkaizen.com>
2018-07-31 23:06:54 +0600

    Display current month and year ...

--- scripts/app.js
+++ scripts/app.js
@@ -147,6 +147,7 @@ var UIController = (function() {
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
         expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month',
     };
     
     var formatNumber = function(num, type) {
@@ -258,6 +259,19 @@ var UIController = (function() {
             
         },
         
+        displayMonth: function () {
+            var now, month, months,year;
+            
+            now = new Date();
+            
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            
+            year = now.getFullYear();
+            
+            document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+        },
+        
         getDOMStrings: function() {
             return DOMStrings;
         },
@@ -363,6 +377,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.');
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
